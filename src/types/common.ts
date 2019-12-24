61ba5f64760589e446a7f1bb0eedb2f099d4fd58
Valentin Touffet <contact@eywek.fr>
2019-12-24 15:11:45 +0100

    feat: unpromisify to avoid double promise

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,15 @@
+// Used to get parameters type from patched function
 export type Parameters<T> = T extends (...args: infer T) => any ? T : unknown[]
-export type ReturnType<T> = T extends (...args: any[]) => infer T ? T : string | object
-export type Func<T> = (...args: Parameters<T>) => ReturnType<T>
+// Used to get return type from patched function used for type our memoize function
+// (we remove the promise, the original function should be an async function)
+export type ReturnType<T> = T extends (...args: any[]) => infer T ? Unpromisify<T> : string | object
+// Used to type the patched function (with the promise)
+export type OriginalReturnType<T> = Promise<ReturnType<T>>
+// Type of the original function with params and promise as result
+export type Func<T> = (...args: Parameters<T>) => OriginalReturnType<T>
+// Type of our memoize function with original params and promise with original returns types
+// we need to remove promise from the original function and set it again to be sure to only
+// have one level of promise (avoid Promise<Promise<string>>)
 export type AsyncFunc<T> = (...args: Parameters<T>) => Promise<ReturnType<T>>
+// Used to remove promise from a type
+export type Unpromisify<T> = T extends Promise<infer R> ? R : T
